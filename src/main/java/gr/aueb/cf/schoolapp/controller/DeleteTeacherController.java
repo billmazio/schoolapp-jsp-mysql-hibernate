c0a2589283f367a1ced6affa64ce7de7f6e3325d
vasilis maziotis <bmaziotis@windowslive.com>
2023-08-28 17:45:25 +0300

    initial file

--- src/main/java/gr/aueb/cf/schoolapp/controller/DeleteTeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/DeleteTeacherController.java
@@ -1,27 +1,18 @@
 package gr.aueb.cf.schoolapp.controller;
 
-
-
-
-
 import gr.aueb.cf.schoolapp.dao.MeetingDAOHibernateImpl;
 import gr.aueb.cf.schoolapp.dao.SpecialtyDAOHibernateImpl;
 import gr.aueb.cf.schoolapp.dao.TeacherDAOHibernateImpl;
 import gr.aueb.cf.schoolapp.dao.dbutil.HibernateHelper;
 import gr.aueb.cf.schoolapp.dao.exceptions.MeetingDAOException;
-import gr.aueb.cf.schoolapp.dao.exceptions.SpecialtyDAOException;
 import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
-import gr.aueb.cf.schoolapp.dto.TeacherDeleteDTO;
 import gr.aueb.cf.schoolapp.model.Meeting;
-import gr.aueb.cf.schoolapp.model.Specialty;
-import gr.aueb.cf.schoolapp.model.Student;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.service.MeetingServiceImpl;
 import gr.aueb.cf.schoolapp.service.SpecialtyServiceImpl;
 import gr.aueb.cf.schoolapp.service.TeacherServiceImpl;
 import gr.aueb.cf.schoolapp.service.exceptions.MeetingNotFoundException;
 import gr.aueb.cf.schoolapp.service.exceptions.TeacherNotFoundException;
-
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -33,7 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
+
 
 @WebServlet("/schoolapp/deleteTeacher")
 public class DeleteTeacherController extends HttpServlet {
@@ -42,83 +33,69 @@ public class DeleteTeacherController extends HttpServlet {
 	private EntityManager entityManager = emf.createEntityManager();
 	private TeacherDAOHibernateImpl teacherDAO = new TeacherDAOHibernateImpl(entityManager);
 	private TeacherServiceImpl teacherService = new TeacherServiceImpl(teacherDAO);
-	private SpecialtyDAOHibernateImpl specialtyDAO = new SpecialtyDAOHibernateImpl(entityManager);
-	private SpecialtyServiceImpl specialtyService = new SpecialtyServiceImpl(specialtyDAO);
 	private MeetingDAOHibernateImpl meetingDAO = new MeetingDAOHibernateImpl(entityManager);
 	private MeetingServiceImpl meetingService = new MeetingServiceImpl(meetingDAO); // Initialize your MeetingService
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//response.setContentType("text/html; charset=UTF-8");
 		int id = Integer.parseInt(request.getParameter("id"));
-		String firstname = request.getParameter("firstname");
-		String lastname = request.getParameter("lastname");
-		String specialtyName = request.getParameter("specialty");
 
 
-		Specialty specialty = null;
-		if (specialtyName != null && !specialtyName.trim().isEmpty()) {
+		Teacher teacherToDelete = null;
+		try {
+			teacherToDelete = teacherService.getTeacherById(id);
+
+
+		} catch (TeacherNotFoundException | TeacherDAOException e) {
+			handleError(request, response, e.getMessage());
+			return;
+		}
+
+
+
+		List<Meeting> teacherMeetings = teacherToDelete.getMeetings();
+
+		if (teacherMeetings != null) {
 			try {
-				Optional<List<Specialty>> specialtiesOptional = specialtyDAO.getSpecialtyByName(specialtyName);
-				if (specialtiesOptional.isPresent() && !specialtiesOptional.get().isEmpty()) {
-					specialty = specialtiesOptional.get().get(0);
-				} else {
-					request.setAttribute("deleteAPIError", true);
-					request.setAttribute("message", "Specialty not found.");
-					request.getRequestDispatcher("/school/static/templates/teachers.jsp")
-							.forward(request, response);
+				teacherMeetings.stream()
+						.forEach(meeting -> {
+							try {
+								meetingService.deleteMeeting(meeting.getId());
+							} catch (MeetingDAOException | MeetingNotFoundException e) {
+								throw new RuntimeException(e);
+							}
+						});
+			} catch (RuntimeException e) {
+				Throwable cause = e.getCause();
+				if (cause instanceof MeetingDAOException || cause instanceof MeetingNotFoundException) {
+					handleError(request, response, cause.getMessage());
 					return;
 				}
-			} catch (SpecialtyDAOException e) {
-				request.setAttribute("deleteAPIError", true);
-				request.setAttribute("message", "Failed to retrieve specialty.");
-				request.getRequestDispatcher("/school/static/templates/teachers.jsp")
-						.forward(request, response);
-				return;
 			}
 		}
 
 		try {
-			// Retrieve the student to be deleted
-			Teacher teacherToDelete = teacherService.getTeacherById(id);
+			// Assuming transaction is started in deleteTeacher and deleteMeeting
+			teacherService.deleteTeacher(id);
 
 
-			// Retrieve related meetings of the student
-			List<Meeting> teacherMeetings = teacherToDelete.getMeetings();
-
-			// Delete each related meeting first
-			for (Meeting meeting : teacherMeetings) {
-				meetingService.deleteMeeting(meeting.getId());
-				HibernateHelper.getEntityManager().clear();
-
-			}
-
-
-			TeacherDeleteDTO teacherDTO = new TeacherDeleteDTO();
-			teacherDTO.setId(id);
-			teacherDTO.setFirstname(firstname);
-			teacherDTO.setLastname(lastname);
-			teacherDTO.setSpecialty(specialty);
-
-				teacherService.deleteTeacher(id);
-			HibernateHelper.getEntityManager().clear();
-
-				request.setAttribute("teacherDTO", teacherDTO);
-				request.getRequestDispatcher("/school/static/templates/teacherDeleted.jsp")
-						.forward(request, response);
-			} catch (TeacherNotFoundException | TeacherDAOException | MeetingDAOException | MeetingNotFoundException e) {
-				request.setAttribute("deleteAPIError", true);
-				request.setAttribute("message", e.getMessage());
-				request.getRequestDispatcher("/school/static/templates/teachers.jsp")
-						.forward(request, response);
-			}
+			request.setAttribute("teacherDTO", teacherToDelete);
+			request.getRequestDispatcher("/school/static/templates/teacherDeleted.jsp").forward(request, response);
+		} catch (TeacherDAOException | TeacherNotFoundException e) {
+			handleError(request, response, e.getMessage());
 		}
+	}
 
-			@Override
-			public void destroy () {
-				HibernateHelper.closeEntityManager();
-				HibernateHelper.closeEMF();
-			}
-		}
-
+	private void handleError(HttpServletRequest request, HttpServletResponse response, String message)
+			throws ServletException, IOException {
+		request.setAttribute("deleteAPIError", true);
+		request.setAttribute("message", message);
+		request.getRequestDispatcher("/school/static/templates/teachers.jsp").forward(request, response);
+	}
+	@Override
+	public void destroy () {
+		HibernateHelper.closeEntityManager();
+		HibernateHelper.closeEMF();
+	}
+}
